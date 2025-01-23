c249580e8e7006095737a014f70eeeb1256394f6
Quinn Webber <46348512+qwebber@users.noreply.github.com>
2025-01-23 09:47:26 -0500

    Update 03-networks.R

--- code/03-networks.R
+++ code/03-networks.R
@@ -3,15 +3,19 @@ library(data.table)
 library(spatsoc)
 
 fogo <- fread("output/fogo-gsp.csv")
+fogo$herd <- "FOGO"
+mr <- fread("output/mr-gsp.csv")
+
+all <- rbind(fogo, mr, fill = T)
 
 ###### GENERATE NETWORKS FOR OBSERVED DATA ######
 
-fogo <- group_times(fogo, datetime = 'datetime', threshold = '5 minutes')
+all <- group_times(all, datetime = 'datetime', threshold = '5 minutes')
 
-fogo <- group_pts(
-	fogo,
+all <- group_pts(
+	all,
 	threshold = 50,
-	splitBy = c('yr', 'season', 'tod', 'Cover'),
+	splitBy = c('yr', 'season', 'tod', 'Cover', 'herd'),
 	timegroup = 'timegroup',
 	id = 'id',
 	coords = c('x_proj', 'y_proj')
@@ -20,8 +24,8 @@ fogo <- group_pts(
 ### Calculate network metrics
 source("functions/dynamic_network.R")
 
-nets <- dynamic_network(fogo,
+nets <- dynamic_network(all,
 												id = 'id',
-												by = c('yr', 'season', 'tod', 'Cover'))
+												by = c('yr', 'season', 'tod', 'Cover', 'herd'))
 
 fwrite(nets, "output/tod-networks.csv")
